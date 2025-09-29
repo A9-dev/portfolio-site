a81d05296249faab4370f83bf89e9d5db2eb7d91
A9-dev <henrypearson422@gmail.com>
2025-09-29 08:53:42 +0100

    feat: implement pages and first blog content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,20 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
 
+// 2. Import loader(s)
+import { glob, file } from "astro/loaders";
+
+// 3. Define your collection(s)
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-		}),
+  loader: glob({ pattern: "**/*.(md|mdx)", base: "./src/data/blog" }),
+  schema: z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
 });
 
+// 4. Export a single `collections` object to register your collection(s)
 export const collections = { blog };
